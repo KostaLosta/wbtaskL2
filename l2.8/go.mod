f63e56781698a07fd5c7216fbe0e8655bd286c22
Dmitrii Kistirev <kistirev@toweco.ru>
2026-02-10 20:58:26 +0300

    new

---
+++ l2.8/go.mod
@@ -0,0 +1,10 @@
+module github.com/KostaLosta/wbtaskL2/l2.8
+
+go 1.24.0
+
+require github.com/beevik/ntp v1.5.0
+
+require (
+	golang.org/x/net v0.44.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
+)
--- l2.8/main.go
+++ l2.8/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/beevik/ntp"
+)
+
+func main() {
+	currentTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Println(currentTime)
+}
+
+// Документация пакета https://pkg.go.dev/github.com/beevik/ntp#section-readme
